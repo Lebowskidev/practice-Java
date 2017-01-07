29f684dfecacc177457196ed9cb805755ced48d9
Lebowskidev <andriy.auditore@gmail.com>
2017-01-07 19:41:46 +0200

    Write new function

--- src/Base.java
+++ src/Base.java
@@ -7,17 +7,33 @@ public class Base {
 	 * @param args
 	 */
 	
+	public static Scanner in = new Scanner(System.in);
+	
 	public static void strwork(){
-    	Scanner in = new Scanner(System.in);
 		String str = "";
-		System.out.println("Pleas input data");
-		str = in.next();
-		System.out.print(str);
+		System.out.println("Write your name");
+		str = in.nextLine();
+		System.out.print("Hello "+ str);
 	}
+	
+	public static void massive(){
+		int[] mas = new int [10];
+		
+		for(int i = 0; i < 10;i++){
+			mas[i] = i;
+			System.out.print(" " + mas[i]);
+		}
+		System.out.println("  length = " + mas.length);
+		
+	}
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		strwork();
+		massive();
+		
 	}
 
 }
