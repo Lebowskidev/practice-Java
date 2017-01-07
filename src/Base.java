66f288f797db98d55ce9c79e8cddc2cdbb83bdf0
Lebowskidev <andriy.auditore@gmail.com>
2017-01-07 17:28:40 +0200

    Create the project and start work

---
+++ src/Base.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Base {
+
+	/**
+	 * @param args
+	 */
+
+    private Scanner in = new Scanner(System.in);
+	
+	public void stringwork(String str){
+		System.out.println("Pleas input data");
+		str = in.next();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
