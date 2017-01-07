8005b3966ba42a46703258c3c6b43391bdbe1fc2
Lebowskidev <andriy.auditore@gmail.com>
2017-01-07 17:34:08 +0200

    Create function input and output data

--- src/Base.java
+++ src/Base.java
@@ -6,17 +6,18 @@ public class Base {
 	/**
 	 * @param args
 	 */
-
-    private Scanner in = new Scanner(System.in);
 	
-	public void stringwork(String str){
+	public static void strwork(){
+    	Scanner in = new Scanner(System.in);
+		String str = "";
 		System.out.println("Pleas input data");
 		str = in.next();
+		System.out.print(str);
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		strwork();
 	}
 
 }
